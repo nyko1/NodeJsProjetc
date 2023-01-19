91cf83ea51ec4e38f256f05d15c652ca43b01a55
Ange <angepatrickgoprou@gmail.com>
2023-01-19 08:11:39 +0000

    ajout controlleur pour la recherche par nom

--- controllers/search.js
+++ controllers/search.js
@@ -1,23 +1,18 @@
-// const db = require('../config/mysqlConn')
-const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args))
+const axios = require('axios').default
 
 const getUserByName = async (req, res) => {
   try {
-    const res = await fetch('http://192.168.252.203:8096/participants', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
+    const { data } = await axios.post(
+      'http://192.168.252.203:8096/participants',
+      {
         page: 0,
         size: 224,
-        recherche: req.body?.recherche || '',
+        recherche: req.body.recherche || '',
         departementCode: 'string',
         programmes: ['string'],
-      }),
-    }).then((data) => {
-      return data.json()
-    })
-    res.status(200).json(res)
+      }
+    )
+    res.status(200).json(data)
   } catch (err) {
     res.status(400).json(err)
   }
