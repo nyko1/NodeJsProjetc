4668a664bfb6d3508a39a7951d001cdf1433ea15
Ange <angepatrickgoprou@gmail.com>
2023-01-19 10:06:00 +0000

    mise à jour syntaxique

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const projetsRoutes = require('./routes/projets')
 const searchRoutes = require('./routes/search')
-const equipeRoutes = require('./routes/equipes')
+const equipesRoutes = require('./routes/equipes')
 
 const app = express()
 
@@ -12,6 +12,6 @@ app.use(express.json())
 
 app.use('/api/projets', projetsRoutes)
 app.use('/api/search', searchRoutes)
-app.use('/api/equipes', equipeRoutes)
+app.use('/api/equipes', equipesRoutes)
 
 module.exports = app
