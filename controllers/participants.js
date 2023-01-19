5194fb2138aa9d8d3f2d62fdae3230b025a436eb
Ange <angepatrickgoprou@gmail.com>
2023-01-19 11:24:34 +0000

    ajout du controlleur getAll

---
+++ controllers/participants.js
@@ -0,0 +1,21 @@
+const axios = require('axios').default
+
+const getAll = async (req, res) => {
+  try {
+    const { data } = await axios.post(
+      'http://192.168.252.203:8096/participants',
+      {
+        page: 0,
+        size: 224,
+        recherche: 'string',
+        departementCode: 'string',
+        programmes: ['string'],
+      }
+    )
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(400).json(err)
+  }
+}
+
+module.exports = { getAll }
